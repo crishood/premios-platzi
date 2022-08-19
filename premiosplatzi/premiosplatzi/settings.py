9e5b9c80f26d9b3dbf64bd177cbc29baa7b8b313
crishood <crishood13@gmail.com>
2022-08-19 11:26:46 -0500

    Create polls model and first migration

--- premiosplatzi/premiosplatzi/settings.py
+++ premiosplatzi/premiosplatzi/settings.py
@@ -30,14 +30,21 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+)
+
+LOCAL_APPS = (
+    'polls',
+)
+
+
+INSTALLED_APPS = DJANGO_APPS  + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -105,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC-05:00'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
