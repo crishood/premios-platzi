86a046beb8b7e8fb897b9858bf67cb832f18830e
crishood <crishood13@gmail.com>
2022-08-22 07:59:12 -0500

    Create index and detail view and template

--- premiosplatzi/polls/views.py
+++ premiosplatzi/polls/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello ma G")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
+
+
+def results(results, question_id):
+    return HttpResponse(f"This is {question_id} results")
+
+
+def vote(results, question_id):
+    return HttpResponse(f"You are voting {question_id}")
